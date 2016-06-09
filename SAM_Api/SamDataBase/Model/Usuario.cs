b78f9cd0276b5f0228f376d7e0b96ec83f0d41c2
Jesley Marcelino <jesley.caceres.marcelino@gmail.com>
2016-06-09 17:07:08 -0300

    atualização do model de usuario

--- SAM_Api/SamDataBase/Model/Usuario.cs
+++ SAM_Api/SamDataBase/Model/Usuario.cs
@@ -32,6 +32,7 @@ namespace SamDataBase.Model
         public System.DateTime dataInicio { get; set; }
         public string foto { get; set; }
         public bool ativo { get; set; }
+        public string perfil { get; set; }
     
         public virtual Cargo Cargo { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
