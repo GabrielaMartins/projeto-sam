189ab0612059bdf14b6410726450a74ad7e24759
Jesley Marcelino <jesley.caceres.marcelino@gmail.com>
2016-08-30 14:48:45 -0300

    deletando arquivos antigos

--- SAM_Database/SAM-Itens.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Itens VALUES ('Java Programmer', 'Oracle Certified Professional, Java SE 7 Programmer', 8, 3, 1);
-INSERT INTO Itens VALUES ('AngularJS', 'Curso AngularJS para WebApps RESTful', 3, 3, 2);
-INSERT INTO Itens VALUES ('AWS', 'Amazon Web Services, também conhecido como AWS, é uma coleção de serviços de computação em nuvem ou serviços web, que formam uma plataforma de computação na nuvem oferecida por Amazon.com.', 8, 3, 7);
