41a519d1c7e73b9c331325aa96f5f53858d407bf
Jesley Marcelino <jesley.caceres.marcelino@gmail.com>
2016-09-20 14:09:36 -0300

    nova action para promocao do usuario

--- SAM_Api/SamApiModels/Models/User/PromocaoUsuarioViewModel.cs
+++ SAM_Api/SamApiModels/Models/User/PromocaoUsuarioViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using SamModelValidationRules.Attributes.Validation;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SamApiModels.Models.User
+namespace SamApiModels.User
 {
     /// <summary>
     /// Representa os dados para promover um usuário
